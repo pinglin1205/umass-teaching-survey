633f013efbaa10163baa54a26ff07800d6bb13be
pinglin1205 <116209154+pinglin1205@users.noreply.github.com>
2023-10-21 07:45:51 +0000

    first interface

--- web.py
+++ web.py
@@ -1,35 +1,38 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template,jsonify,url_for
 
 app = Flask(__name__)
-JOBS=[
-  {
-    'id':1,
-    'title':'Python Developer',
-    'location':'New York',
-    'salary':50000
-  },
-  {
-    'id':2,
-    'title':'Data Scientist',
-    'location':'Terre Haute',
-    'salary':60000
-  },
-  {
-    'id':3,
-    'title':'Frontend Engineer',
-    'location':'Remote',
-    'salary':80000
-  }
+# JOBS=[
+#   {
+#     'id':1,
+#     'title':'Python Developer',
+#     'location':'New York',
+#     'salary':50000
+#   },
+#   {
+#     'id':2,
+#     'title':'Data Scientist',
+#     'location':'Terre Haute',
+#     'salary':60000
+#   },
+#   {
+#     'id':3,
+#     'title':'Frontend Engineer',
+#     'location':'Remote',
+#     'salary':80000
+#   }
   
-]
+# ]
 
 @app.route("/")
 def first_web():
-    return render_template('home.html',jobs=JOBS)
+    return render_template('home.html')
   
-@app.route("/api/jobs")
-def list_jobs():
-  return jsonify(JOBS)
+# @app.route("/api/jobs")
+# def list_jobs():
+#   return jsonify(JOBS)
+@app.route("/thank-you")
+def thank_you():
+    return render_template('thank-you.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
